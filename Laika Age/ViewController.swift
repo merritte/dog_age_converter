e5e16920542a30d654f0eeec16286ea0daad4c5f
Shay-Jahen Merritte <Shay@Merritte.com>
2014-10-06 22:17:30 -0400

    adjusted the age algorithm

--- Laika Age/ViewController.swift
+++ Laika Age/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var dogAgeTextField: UITextField!
-    @IBOutlet weak var dogYearsLabel: UILabel!
     
+    @IBOutlet var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +26,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func dogAgePressed(sender: AnyObject) {
-        let stringFromTextField = dogAgeTextField.text
-        let optionalIntFromTextField = stringFromTextField.toInt()
-        let intFromOptional = optionalIntFromTextField!
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = enterHumanYearsTextField.text
+        let optionalDoubleFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        var realDogYears:Double
         
-        dogYearsLabel.text = "\(intFromOptional * 7)"
+        if optionalDoubleFromTextField > 2 {
+            realDogYears = (10.5 * 2) + (optionalDoubleFromTextField - 2) * 4
+        }
+        else {
+            realDogYears = optionalDoubleFromTextField * 10.5
+        }
+        
+        dogYearsLabel.text = "\(realDogYears)"
         dogYearsLabel.hidden = false
-        dogAgeTextField.resignFirstResponder()
+        enterHumanYearsTextField.resignFirstResponder()
+        
     }
-
 }
 
+
+
+//
+//    @IBAction func dogAgePressed(sender: AnyObject) {
+//        let stringFromTextField = dogAgeTextField.text
+//        let optionalIntFromTextField = stringFromTextField.toInt()
+//        let intFromOptional = optionalIntFromTextField!
+//
+//
+//        if int
+//        dogYearsLabel.text = "\(intFromOptional * 7)"
+//        dogYearsLabel.hidden = false
+//        dogAgeTextField.resignFirstResponder()
+//    }
